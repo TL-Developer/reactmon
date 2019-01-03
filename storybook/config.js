33fb213abb10ead65ac8ed676abcc71ee6407b01
Tiago Lima <tiago.lima@xpi.com.br>
2019-01-03 19:15:58 -0200

    Estilizando

--- storybook/config.js
+++ storybook/config.js
@@ -2,6 +2,13 @@ import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
 // const req = require.context('../stories', true, /.stories.js$/);
+
+import '../src/index.scss'
+import '../src/components/Footer/style.scss'
+import '../src/components/Header/style.scss'
+import '../src/components/PokemonDetail/style.scss'
+import '../src/components/Pokemons/style.scss'
+
 const req = require.context('../src/components', true, /.story.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
